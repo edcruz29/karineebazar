85c53ac629c56266e4e15e77ae8ff1aaf8c5bcec
Eduardo <galvaocruz16@gmail.com>
2022-04-02 15:32:45 -0300

    estrutura básica ok

---
+++ public/js/carousel.js
@@ -0,0 +1,14 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel() {
+  idx++;
+
+  if (idx > img.length - 1) {
+    idx = 0;
+  }
+  imgs.style.transform = `translateX(${-idx * 1000}px)`;
+}
+setInterval(carrossel, 1800);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+router.get("/produtos", function (req, res, next) {
+  res.render("produtos");
+});
+router.get("/promocoes", function (req, res, next) {
+  res.render("promocoes");
+});
+router.get("/sobre", function (req, res, next) {
+  res.render("sobre");
 });
 
 module.exports = router;
