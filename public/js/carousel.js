d64f6c5869d3c5f7bf6051778c96c81782fc0d91
Eduardo <galvaocruz16@gmail.com>
2022-04-16 15:24:37 -0300

    promocoes,footer e ajuste no grid

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -9,6 +9,6 @@ function carrossel() {
   if (idx > img.length - 1) {
     idx = 0;
   }
-  imgs.style.transform = `translateX(${-idx * 1600}px)`;
+  imgs.style.transform = `translateX(${-idx * 1300}px)`;
 }
-setInterval(carrossel, 1500);
+setInterval(carrossel, 2000);
